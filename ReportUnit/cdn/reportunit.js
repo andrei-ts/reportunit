fed3e8d036a9a07daa1a91f049f45661fe9173d9
andy-ts <andrei_turavets@epam.com>
2019-06-10 11:22:13 +0300

    revert reportunit.js

--- ReportUnit/cdn/reportunit.js
+++ ReportUnit/cdn/reportunit.js
@@ -4,12 +4,12 @@ function showDynamicModal(heading, content) {
     var m = $('#dynamicModal');
     m.find('h4').text(heading);
     m.find('pre').html(content);
-    m.modal('open');
+    m.openModal({ in_duration: 200 });
 }
 
-$('.details-container').click(function(evt) {
+$('.details-container').click(function (evt) {
     var t = $(evt.target);
-    
+
     if (t.is('.showStatusMessage') || t.is('i')) {
         if (t.is('i')) {
             t = t.parent();
@@ -17,92 +17,92 @@ $('.details-container').click(function(evt) {
         /* Converting brackets*/
         var escaped = t.next().html().replace(/(&lt;)/g, "<");
         escaped = escaped.replace(/(&gt;)/g, ">");
-        showDynamicModal(t.closest('tr').find('.method-name').text() + ' Logs', escaped);
+        showDynamicModal(t.closest('tr').find('.name').text() + ' Logs', escaped);
     }
-    
+
     if (t.is('.showDescription')) {
         showDynamicModal(t.text() + ' Description', t.next().text());
     }
 });
 
 /* toggle dashboard on 'Enable Dashboard' click */
-$('#enableDashboard').click(function() {
+$('#enableDashboard').click(function () {
     $('.suite-list, .suite-details').toggleClass('v-spacer');
     $(this).toggleClass('enabled').children('i').toggleClass('active');
     $('.dashboard').toggleClass('hide');
 });
 
 /* show suite data on click */
-$('.suite').click(function() {
+$('.suite').click(function () {
     var t = $(this);
-    
+
     $('.suite').removeClass('active');
     $('.suite-name-displayed, .details-container').html('');
-    
+
     t.toggleClass('active');
     var html = t.find('.suite-content').html();
-    
+
     $('.suite-name-displayed').text(t.find('.suite-name').text());
     $('.details-container').append(html);
 });
 
-$('#slide-out .report-item > a').filter(function(){
+$('#slide-out .report-item > a').filter(function () {
     return this.href.match(/[^\/]+$/)[0] == document.location.pathname.match(/[^\/]+$/)[0];
 }).parent().addClass('active');
 
 /* filters -> by suite status */
-$('#suite-toggle li').click(function() {
+$('#suite-toggle li').click(function () {
     var t = $(this);
-    
+
     if (!t.hasClass('clear')) {
         resetFilters();
-        
+
         var status = t.text().toLowerCase();
-        
+
         $('#suites .suite').addClass('hide');
         $('#suites .suite.' + status).removeClass('hide');
-        
+
         selectVisSuite()
     }
 });
 
 /* filters -> by test status */
-$('#tests-toggle li').click(function() {
+$('#tests-toggle li').click(function () {
     var t = $(this);
 
     if (!t.hasClass('clear')) {
         resetFilters();
-        
+
         var opt = t.text().toLowerCase();
-        
+
         $('.suite table tr.test-status:not(.' + opt + '), .details-container tr.test-status:not(.' + opt).addClass('hide');
         $('.suite table tr.test-status.' + opt + ', .details-container tr.test-status.' + opt).removeClass('hide');
-        
+
         hideEmptySuites();
         selectVisSuite()
     }
 });
 
 /* filters -> by category */
-$('#category-toggle li').click(function() {
+$('#category-toggle li').click(function () {
     var t = $(this);
 
     if (!t.hasClass('clear')) {
         resetFilters();
-        
+
         filterByCategory(t.text());
         selectVisSuite()
     }
 });
 
-$('.clear').click(function() {
+$('.clear').click(function () {
     resetFilters(); selectVisSuite()
 });
 
 function filterByCategory(cat) {
     resetFilters();
 
-    $('td.test-features').each(function() {
+    $('td.test-features').each(function () {
         if (!($(this).hasClass(cat))) {
             $(this).closest('tr').addClass('hide');
         }
@@ -112,9 +112,9 @@ function filterByCategory(cat) {
 }
 
 function hideEmptySuites() {
-    $('.suite').each(function() {
+    $('.suite').each(function () {
         var t = $(this);
-        
+
         if (t.find('tr.test-status').length == t.find('tr.test-status.hide').length) {
             t.addClass('hide');
         }
@@ -134,42 +134,38 @@ function clickListItem(listClass, index) {
     $('#' + listClass).find('li').get(index).click();
 }
 
-$(document).ready(function() {
-	/* init */
-	$('select').formSelect();
-	$('.modal-trigger').modal();
-	$('.tooltipped').tooltip({delay: 10});
+$(document).ready(function () {
+    /* init */
+    $('select').material_select();
+    $('.modal-trigger').leanModal();
+    $('.tooltipped').tooltip({ delay: 10 });
 
-	/* for a single report item, hide sidenav */
-	if ($('.report-item').length <= 1) {
-		$('#slide-out').addClass('hide');
-		
-		pinWidth = '56.5%';
-		
-		$('.pin').css('width', pinWidth);
-		$('.main-wrap, nav').css('padding-left', '20px');
-	}
+    /* for a single report item, hide sidenav */
+    if ($('.report-item').length <= 1) {
+        $('#slide-out').addClass('hide');
 
-	var passedPercentage = Math.round(((passed / (passed + failed)) * 100)) + '%';
-	$('.pass-percentage').text(passedPercentage);
-	$('.dashboard .determinate').attr('style', 'width:' + passedPercentage);
+        pinWidth = '56.5%';
 
-	testsChart();
-	$('ul.doughnut-legend').addClass('right');
-	
-	resetFilters();
+        $('.pin').css('width', pinWidth);
+        $('.main-wrap, nav').css('padding-left', '20px');
+    }
+
+    var passedPercentage = Math.round(((passed / (passed + failed)) * 100)) + '%';
+    $('.pass-percentage').text(passedPercentage);
+    $('.dashboard .determinate').attr('style', 'width:' + passedPercentage);
+
+    testsChart();
+    $('ul.doughnut-legend').addClass('right');
+
+    resetFilters();
     $('.suite:first-child').click();
-    $('.collapsible').collapsible({
-        accordion: false
-    });
-
 });
 
 var options = {
-	segmentShowStroke : false, 
-	percentageInnerCutout : 55, 
-	animationSteps : 1,
-	legendTemplate : '<ul class=\'<%=name.toLowerCase()%>-legend\'><% for (var i=0; i<segments.length; i++){%><li><span style=\'background-color:<%=segments[i].fillColor%>\'></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>'
+    segmentShowStroke: false,
+    percentageInnerCutout: 55,
+    animationSteps: 1,
+    legendTemplate: '<ul class=\'<%=name.toLowerCase()%>-legend\'><% for (var i=0; i<segments.length; i++){%><li><span style=\'background-color:<%=segments[i].fillColor%>\'></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>'
 };
 
 /* report -> suites chart */
@@ -210,64 +206,64 @@ var skipped = $('td.skipped').length;
 
 /* report -> tests chart */
 function testsChart() {
-	if (!$('#test-analysis').length) {
-		return false;
-	}
+    if (!$('#test-analysis').length) {
+        return false;
+    }
 
     var data = {};
-    
+
     if ($('body.summary').length > 0) {
         total = parseInt($('#total-tests').text());
         passed = parseInt($('#total-passed').text());
         failed = parseInt($('#total-failed').text());
         others = parseInt($('#total-others').text());
-        
+
         data = [
             { value: passed, color: '#00af00', highlight: '#32bf32', label: 'Pass' },
-            { value: failed, color:'#F7464A', highlight: '#FF5A5E', label: 'Fail' },
+            { value: failed, color: '#F7464A', highlight: '#FF5A5E', label: 'Fail' },
             { value: others, color: '#1e90ff', highlight: '#4aa6ff', label: 'Skipped' }
         ];
-        
+
         $('.test-others-count').text(others);
     }
     else {
         data = [
             { value: passed, color: '#00af00', highlight: '#32bf32', label: 'Pass' },
-            { value: failed, color:'#F7464A', highlight: '#FF5A5E', label: 'Fail' },
+            { value: failed, color: '#F7464A', highlight: '#FF5A5E', label: 'Fail' },
             { value: skipped, color: '#1e90ff', highlight: '#4aa6ff', label: 'Skipped' }
         ];
-        
+
         $('.test-others-count').text(errors + inconclusive + skipped);
     }
-	
-	$('.test-pass-count').text(passed);
-	$('.test-fail-count').text(failed);
-	
-	var ctx = $('#test-analysis').get(0).getContext('2d');
-	testChart = new Chart(ctx).Doughnut(data, options);
-	drawLegend(testChart, 'test-analysis');
+
+    $('.test-pass-count').text(passed);
+    $('.test-fail-count').text(failed);
+
+    var ctx = $('#test-analysis').get(0).getContext('2d');
+    testChart = new Chart(ctx).Doughnut(data, options);
+    drawLegend(testChart, 'test-analysis');
 }
 
 /* draw legend for test and step charts [DASHBOARD] */
 function drawLegend(chart, id) {
-	var helpers = Chart.helpers;
-	var legendHolder = document.getElementById(id);
-	
-	legendHolder.innerHTML = chart.generateLegend();
-	
-	helpers.each(legendHolder.firstChild.childNodes, function(legendNode, index) {
-		helpers.addEvent(legendNode, 'mouseover', function() {
-			var activeSegment = chart.segments[index];
-			activeSegment.save();
-			activeSegment.fillColor = activeSegment.highlightColor;
-			chart.showTooltip([activeSegment]);
-			activeSegment.restore();
-		});
-	});
-	
-	Chart.helpers.addEvent(legendHolder.firstChild, 'mouseout', function() {
-		chart.draw();
-	});
-	
-	$('#' + id).after(legendHolder.firstChild);
+    var helpers = Chart.helpers;
+    var legendHolder = document.getElementById(id);
+
+    legendHolder.innerHTML = chart.generateLegend();
+
+    helpers.each(legendHolder.firstChild.childNodes, function (legendNode, index) {
+        helpers.addEvent(legendNode, 'mouseover', function () {
+            var activeSegment = chart.segments[index];
+            activeSegment.save();
+            activeSegment.fillColor = activeSegment.highlightColor;
+            chart.showTooltip([activeSegment]);
+            activeSegment.restore();
+        });
+    });
+
+    Chart.helpers.addEvent(legendHolder.firstChild, 'mouseout', function () {
+        chart.draw();
+    });
+
+    $('#' + id).after(legendHolder.firstChild);
 }
